6b505c604fd8c538fc4e9d1425fbb9cbdf18bb59
Bownuz <fedde.snelder@gmail.com>
2024-04-05 11:22:32 +0200

    Tutorial video nummer 6 af. Custom items kunnen nu durability hebben.
    https://www.youtube.com/watch?v=TPfNvwfgXAU&list=PLKGarocXCE1H9Y21-pxjt5Pt8bW14twa-&index=6

--- src/main/java/net/fedde/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/fedde/tutorialmod/item/ModCreativeModTabs.java
@@ -22,6 +22,8 @@ public class ModCreativeModTabs {
                         output.accept(ModItems.SAPPHIRE.get());
                         output.accept(ModItems.Raw_SAPPHIRE.get());
 
+                        output.accept(ModItems.METAL_DETECTOR.get());
+
                         output.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                         output.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
 
--- src/main/java/net/fedde/tutorialmod/item/ModItems.java
+++ src/main/java/net/fedde/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.fedde.tutorialmod.item;
 
 import net.fedde.tutorialmod.TutorialMod;
+import net.fedde.tutorialmod.item.custom.MetalDetectorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,6 +18,10 @@ public class ModItems {
     public static final RegistryObject<Item> Raw_SAPPHIRE =
             ITEMS.register("raw_sapphire",
                     () -> new Item(new Item.Properties()));
+  public static final RegistryObject<Item> METAL_DETECTOR =
+          ITEMS.register("metal_detector",
+                  () -> new MetalDetectorItem(new Item.Properties().durability(100)));
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
