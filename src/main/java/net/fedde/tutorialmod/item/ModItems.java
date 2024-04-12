49a17b652642e5edbd21a12b837c123509156ed3
Bownuz <fedde.snelder@gmail.com>
2024-04-12 10:53:11 +0200

    Tutorial video nummer 17 af. Willekeurige effects kunnen nu op armor gezet worden
    https://www.youtube.com/watch?v=TKu7gLttXbs&list=PLKGarocXCE1H9Y21-pxjt5Pt8bW14twa-&index=17

--- src/main/java/net/fedde/tutorialmod/item/ModItems.java
+++ src/main/java/net/fedde/tutorialmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.fedde.tutorialmod.item;
 
 import net.fedde.tutorialmod.TutorialMod;
 import net.fedde.tutorialmod.item.custom.MetalDetectorItem;
+import net.fedde.tutorialmod.item.custom.ModArmorItem;
 import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -34,7 +35,7 @@ public class ModItems {
             () -> new HoeItem(ModToolTiers.SAPPHIRE, 0, 0, new Item.Properties()));
 
     public static final RegistryObject<Item> SAPPHIRE_HELMET = ITEMS.register("sapphire_helmet",
-            () -> new ArmorItem(ModArmorMaterials.SAPPHIRE, ArmorItem.Type.HELMET, new Item.Properties()));
+            () -> new ModArmorItem(ModArmorMaterials.SAPPHIRE, ArmorItem.Type.HELMET, new Item.Properties()));
     public static final RegistryObject<Item> SAPPHIRE_CHESTPLATE = ITEMS.register("sapphire_chestplate",
             () -> new ArmorItem(ModArmorMaterials.SAPPHIRE, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
     public static final RegistryObject<Item> SAPPHIRE_LEGGINGS = ITEMS.register("sapphire_leggings",
